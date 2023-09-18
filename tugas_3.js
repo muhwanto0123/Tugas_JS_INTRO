30ea6af257b9b996d287f6dc4e3f25bafeb92484
muhwanto0123 <55321001+muhwanto0123@users.noreply.github.com>
2023-09-18 17:56:56 +0700

    js

--- tugas_3.js
+++ tugas_3.js
@@ -1,7 +1,7 @@
 const printSegitiga = 7;
 for (let line = 1; line <= printSegitiga; i++) {
   let row = '';
-  for (let number = 1; number <= i; number++) {
+  for (let number = 1; number <= line; number++) {
     row += number;
   }
   console.log(row);
